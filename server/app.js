072697a8e3dac7bcf09324ab1c7c2158cbeb9519
Enejo James <spec.en.james@gmail.com>
2018-07-10 16:29:58 +0100

    Still setting up

--- server/app.js
+++ server/app.js
@@ -1,6 +1,24 @@
-import bcrypt from 'bcrypt';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import morgan from 'morgan';
+
+import Routes from './routes/Routes';
 
 const app = express();
+const urlencoded = bodyParser.urlencoded({ extended: false });
+const json = bodyParser.json();
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+app.set('view engine', 'ejs');
+
+app.use(urlencoded);
+app.use(json);
+app.use(morgan('combined'));
+app.use(cookieParser());
+
+
+app.use('/', Routes);
+app.use('*', (req, res) => res.render('error'));
+
+app.listen(port);
